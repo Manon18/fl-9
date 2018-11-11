44cb69ab7872166247e9436d56d899df16801085
Manon18 <maryland16z18@gmail.com>
2018-11-11 21:16:34 +0200

    added app folder with angular homework

---
+++ FL_9_14_homework_angular-part-1/homework/src/app/app.constants.js
@@ -0,0 +1,38 @@
+const wallPosts = [
+  {
+    title: 'New car',
+    message: 'I have got a new car!!! Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age. Shy resolution instrument unreserved man few. She did open find pain some out. If we landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by it admitting cordially september newspaper an. Acceptance middletons am it favourable. It it oh happen lovers afraid.',
+    author_name: 'Oleh Hugo',
+    author_id: 11,
+    author_photo: './assets/img/oleh_hugo.jpg',
+    liked_by: [
+      { name: 'Wendy Marvell', id: 1},
+      { name: 'Gray Fullbuster', id: 2}
+    ]
+  },
+   {
+    title: 'Trip to USA',
+    message: 'It was amazing trip to... He an thing rapid these after going drawn or. Timed she his law the spoil round defer. In surprise concerns informed betrayed he learning is ye. Ignorant formerly so ye blessing. He as spoke avoid given downs money on we. Of properly carriage shutters ye as wandered up repeated moreover. Inquietude attachment if ye an solicitude to. Remaining so continued concealed as knowledge happiness. Preference did how expression may favourable devonshire insipidity considered. An length design regret an hardly barton mr figure.',
+    author_name: 'George Throg',
+    author_id: 10,
+    author_photo: './assets/img/george_throg.jpg',
+    liked_by: [
+      { name: 'Natasha Milostrone', id: 3},
+      { name: 'Erza Scarlet', id: 4},
+      { name: 'Lucy Heartfilia', id: 5}
+    ],    
+  },
+   {
+    title: 'Story of my life',
+    message: 'I was two years old... Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.',
+    author_name: 'Anna Milon',
+    author_id: 15,
+    author_photo: './assets/img/anna_milon.jpg',
+    liked_by: [
+      { name: 'Natasha Milostrone', id: 3},
+      { name: 'Lamia Scale', id: 4},
+      { name: 'Naruto Uzumaki', id: 7},
+      { name: 'Natsu Dragneel', id: 8}
+    ]
+  }
+];
---
+++ FL_9_14_homework_angular-part-1/homework/src/app/app.controller.js
@@ -0,0 +1,84 @@
+angular.module('postsApp')
+  .controller('postsController', function() {
+    this.user = {
+      name: 'Natasha Milostrone',
+      id: 3,
+      photo: './assets/img/natasha_milostrone.jpg',
+    };
+
+    this.allPosts = wallPosts.map(function(item) {
+      return Object.assign({}, item, { isEditMode: false });
+    });
+
+    this.displayPosts = this.allPosts;
+
+    this.isNewPostsFormVisible = false;
+
+    this.openEditForm = function(index) {
+      let itemToEdit = this.displayPosts[index];
+      itemToEdit.editedTitle = itemToEdit.title;
+      itemToEdit.editedMessage = itemToEdit.message;
+      itemToEdit.isEditMode = true;
+    }
+
+    this.saveEditedPost = function(index) {
+      let itemToEdit = this.displayPosts[index];
+      itemToEdit.title = itemToEdit.editedTitle;
+      itemToEdit.message = itemToEdit.editedMessage;
+      itemToEdit.isEditMode = false;
+    }
+
+    this.savePost = function() {
+      this.allPosts.push({ 
+        title: this.newPostTitle,
+        message: this.newPostMessage, 
+        author_id: this.user.id, 
+        author_name: this.user.name, 
+        author_photo: this.user.photo, 
+        liked_by: [] 
+      });
+
+      this.displayPosts = this.allPosts;
+
+      this.newPostTitle = '';
+      this.newPostMessage = '';
+    }
+
+    this.onSearchChanged = function(searchText) {
+      this.displayPosts = this.allPosts.filter((p) => p.title.toLowerCase().includes(searchText.toLowerCase()));
+    }
+
+    this.like = function (id) {
+      let postToLike = this.displayPosts[id];
+
+       if (!postToLike.liked_by.some((p) => p.id == this.user.id)) {
+        postToLike.liked_by.push(this.user);      
+       } else {
+         for (let i = 0; i < postToLike.liked_by.length; i++) {
+          if (postToLike.liked_by[i].id === this.user.id) {
+            postToLike.liked_by.splice(i, 1);
+          }
+         }
+       }
+      
+    }
+
+    this.isLikedByMe = function(id) {
+      let post = this.displayPosts[id];
+
+      for (let i = 0; i < post.liked_by.length; i++) {
+          if (post.liked_by[i].id === this.user.id) {
+            return true;
+          }
+      }
+
+      return false;
+    }
+
+    this.isMyPost = function(id) {
+      let post = this.displayPosts[id];
+
+      return post.author_id === this.user.id;
+    }
+  });
+  
